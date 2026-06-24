8f2c34e255f2462dc4a265394b660da07da722cb
Olexander <parfenov4589@gmail.com>
2026-06-24 18:02:07 +0200

    make build

---
+++ index.bb68f4eb.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var r=e[l],o=r.cells[1].cloneNode(!0),c=r.cells[r.cells.length-1];r.insertBefore(o,c)}
+//# sourceMappingURL=index.bb68f4eb.js.map
