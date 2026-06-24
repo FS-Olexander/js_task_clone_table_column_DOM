c4b59f330014cb315c1814eaaab09a880ea13310
Olexander <parfenov4589@gmail.com>
2026-06-24 17:55:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,16 @@ const table = document.querySelector('table');
 const rows = table.rows;
 
 for (let i = 0; i < rows.length; i++) {
+  const firstCell = rows[i].cells[0].cloneNode(true);
+
+  rows[i].append(firstCell);
+}
+
+for (let i = 0; i < rows.length; i++) {
   const row = rows[i];
   const positionCell = row.cells[1];
   const clonedCell = positionCell.cloneNode(true);
-  const salaryCell = row.cells[row.cells.length - 1];
+  const lastCell = row.cells[row.cells.length - 1];
 
-  row.insertBefore(clonedCell, salaryCell);
+  row.insertBefore(clonedCell, lastCell);
 }
